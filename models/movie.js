bba3a86e7127707b62e057ae6554be84591a85a9
Amogh <amogh.maheshwari@email.com>
2024-08-28 16:25:48 -0700

    chaged movie model

--- models/movie.js
+++ models/movie.js
@@ -1,10 +1,44 @@
+// const mongoose = require('mongoose');
+
+// const movieSchema = new mongoose.Schema({
+//   title: { type: String, required: true },
+//   genre: { type: [String], required: true },
+//   rating: { type: Number, default: 0 },
+//   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+// });
+
+// const Movie = mongoose.model('Movie', movieSchema);
+// module.exports = Movie;
+
+// models/Movie.js
+
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  genre: { type: [String], required: true },
-  rating: { type: Number, default: 0 },
-  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  title: { 
+    type: String, 
+    required: true, 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  genres: [
+    { type: String, required: true }
+  ],
+  director: [
+    { type: String }
+  ],
+  ratings: {
+    averageRating: { type: Number, default: 0 },
+    ratingCount: { type: Number, default: 0 }
+  },
+  reviews: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
+  ],
+  language: { 
+    type: String 
+  },
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
