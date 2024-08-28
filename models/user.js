6d24fa472fcad13e74881d1ca183a803d56b7a11
Amogh <amogh.maheshwari@email.com>
2024-08-28 16:17:59 -0700

    finished user model

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+  preferences: { type: [String], default: [] }
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
