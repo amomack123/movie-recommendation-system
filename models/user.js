92f39f55f5efc3a8c432b84490039c7a6a1778a0
Amogh <amogh.maheshwari@email.com>
2024-08-28 16:23:41 -0700

    chaged user model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,57 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   email: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+//   watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
+//   preferences: { type: [String], default: [] }
+// });
+
+// userSchema.set('toJSON', {
+//     transform: (document, returnedObject) => {
+//         delete returnedObject.hashedPassword;
+//     }
+// });
+
+// module.exports = mongoose.model('User', userSchema);
+// models/User.js
+
+// models/User.js
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-  watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-  preferences: { type: [String], default: [] }
-});
-
-userSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        delete returnedObject.hashedPassword;
-    }
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true, 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    lowercase: true,
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+  preferences: {
+    genres: [{ type: String }],
+    actors: [{ type: String }],
+    directors: [{ type: String }],
+  },
+  watchlist: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }
+  ],
+  favorites: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }
+  ],
+  reviews: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
+  ],
 });
 
 module.exports = mongoose.model('User', userSchema);
