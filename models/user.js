88e6da2d704543cb3de90d18bc3fb5dc525abb72
Amogh <amogh.maheshwari@email.com>
2024-09-05 18:49:51 -0700

    changes to user model fix

--- models/user.js
+++ models/user.js
@@ -1,57 +1,50 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true },
-//   email: { type: String, required: true, unique: true },
-//   password: { type: String, required: true },
-//   favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-//   watchlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
-//   preferences: { type: [String], default: [] }
-// });
-
-// userSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//         delete returnedObject.hashedPassword;
-//     }
-// });
-
-// module.exports = mongoose.model('User', userSchema);
-// models/User.js
-
-// models/User.js
-
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
-  username: { 
-    type: String, 
-    required: true, 
-    unique: true, 
-  },
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true,
-    lowercase: true,
-  },
-  password: { 
-    type: String, 
-    required: true 
-  },
-  preferences: {
-    genres: [{ type: String }],
-    actors: [{ type: String }],
-    directors: [{ type: String }],
-  },
-  watchlist: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }
-  ],
-  favorites: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }
-  ],
-  reviews: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
-  ],
+    firstname: {
+        type: String,
+        // required: true
+    },
+    lastname: {
+        type: String,
+        // required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        sparse: true,
+        // unique: true,
+        // required: true,
+        lowercase: true,
+        validate: {
+            validator: validator.isEmail,
+            message: 'invalid email format'
+        }
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    preferences: {
+        actors: [{ type: String }],
+        genres: [{
+            type: String,
+            enum: ['Adventure', 'Drama', 'Comedy', 'Science fiction', 'Fantasy', 'Horror', 'Thriller', 'Western', 'Musical']
+        }]
+    },
+    reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+    role: { type: String, enum: ['user', 'admin'], default: 'user' }
+});
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.hashedPassword;
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
