4cf8320341c44d76fe1317767ef23eb06aa4da34
Amogh <amogh.maheshwari@email.com>
2024-09-05 18:50:08 -0700

    changes to movie model

--- models/movie.js
+++ models/movie.js
@@ -1,45 +1,16 @@
-// const mongoose = require('mongoose');
-
-// const movieSchema = new mongoose.Schema({
-//   title: { type: String, required: true },
-//   genre: { type: [String], required: true },
-//   rating: { type: Number, default: 0 },
-//   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
-// });
-
-// const Movie = mongoose.model('Movie', movieSchema);
-// module.exports = Movie;
-
-// models/Movie.js
-
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  title: { 
-    type: String, 
-    required: true, 
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  genre: {
+    type: String,
+    required: true,
+    enum: ['Adventure', 'Drama', 'Comedy', 'Science fiction', 'Fantasy', 'Horror', 'Thriller', 'Western', 'Musical']
   },
-  description: { 
-    type: String, 
-    required: true 
-  },
-  genres: [
-    { type: String, required: true }
-  ],
-  director: [
-    { type: String }
-  ],
-  ratings: {
-    averageRating: { type: Number, default: 0 },
-    ratingCount: { type: Number, default: 0 }
-  },
-  reviews: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Review' }
-  ],
-  language: { 
-    type: String 
-  },
+  actors: [{ type: String }],
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  averageRating: { type: Number, default: 0 }
 });
 
-const Movie = mongoose.model('Movie', movieSchema);
-module.exports = Movie;
+module.exports = mongoose.model('Movie', movieSchema);
