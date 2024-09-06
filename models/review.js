aa4ddb348a81064335bdecf7bf302ef63e2532dc
Amogh <amogh.maheshwari@email.com>
2024-09-05 18:49:20 -0700

    changes to reviews.js

--- models/review.js
+++ models/review.js
@@ -1,29 +1,41 @@
+// const mongoose = require('mongoose');
+
+// const reviewSchema = new mongoose.Schema({
+//   user: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'User', 
+//     required: true 
+//   },
+//   movie: { 
+//     type: mongoose.Schema.Types.ObjectId, 
+//     ref: 'Movie', 
+//     required: true 
+//   },
+//   rating: { 
+//     type: Number, 
+//     required: true,
+//     min: 0,
+//     max: 10 
+//   },
+//   comment: { 
+//     type: String 
+//   },
+//   createdAt: { 
+//     type: Date, 
+//     default: Date.now 
+//   }
+// });
+
+// module.exports = mongoose.model('Review', reviewSchema);
+
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User', 
-    required: true 
-  },
-  movie: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'Movie', 
-    required: true 
-  },
-  rating: { 
-    type: Number, 
-    required: true,
-    min: 0,
-    max: 10 
-  },
-  comment: { 
-    type: String 
-  },
-  createdAt: { 
-    type: Date, 
-    default: Date.now 
-  }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  comment: { type: String, required: true },
+  rating: { type: Number, required: true, min: 0, max: 10 },
+  movie: { type: mongoose.Schema.Types.ObjectId, ref: 'Movie', required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
